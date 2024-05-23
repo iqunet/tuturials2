86d9f2da71e445a3d59903290ec96ba632c533f6
brubbel <5667690+brubbel@users.noreply.github.com>
2024-05-23 23:37:34 +0200

    working on LoRaWAN tutorial - fixes

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Private LoRaWAN to OPC-UA",
         "excerpt":"Mission: LoRaWAN Temperature Data to Python Graph Note: This tutorial uses the iQunet Industrial Edge Server. A demo gateway endpoint is provided for the purpose of this guide. LoRaWAN (Long Range Wide Area Network) is a wireless communication protocol designed for low-power, long-range, and low-data-rate applications. This makes it ideal...","categories": ["blog"],
         "tags": [],
-        "url": "/tuturials2/blog/connecting-sensors-opcua/",
+        "url": "/blog/connecting-sensors-opcua/",
         "teaser": null
       }]
